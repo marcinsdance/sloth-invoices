08c3ffe9e63f55dc140f30a987ebefff4cf02fd2
Marcin <mdancewicz@gmail.com>
2015-12-10 01:07:35 +0100

    controller ready, I think better template is needed

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Invoice;
+use Doctrine\DBAL\Types\IntegerType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class DefaultController extends Controller
@@ -18,6 +23,24 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    /**
+     * @Route("/new-invoice", name="new-invoice")
+     */
+    public function newInvoiceAction(Request $request)
+    {
+        $invoice = new Invoice();
+        $form = $this->createFormBuilder($invoice)
+            ->add('number', TextType::class)
+            ->add('date', DateTimeType::class)
+            ->add('client', IntegerType::class)
+            ->add('submit', SubmitType::class, array('label' => 'Add Invoice'))
+            ->getForm();
+
+        return $this->render('default/new.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 
