6a95755350404f43b80422dfd2fc5b684d4c0e09
Marcin <mdancewicz@gmail.com>
2015-12-28 22:39:21 +0100

    nav styled

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -13,11 +13,22 @@ class Builder implements ContainerAwareInterface
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
-        $menu->addChild('Home', array('route' => 'homepage'));
-        $menu->addChild('Invoices', array('route' => 'invoices'));
-        $menu->addChild('Clients');
+        $menu->setChildrenAttribute('class','nav');
+        $menu->addChild('Home', array('route' => 'homepage'))
+            ->setAttribute('class','nav-item');
+        $menu->addChild('Invoices', array('route' => 'invoices'))
+            ->setAttribute('class','nav-item dropdown')
+            ->setLinkAttribute('class','dropdown-toggle')
+            ->setLinkAttribute('data-toggle','dropdown')
+            ->setChildrenAttribute('class','dropdown-menu');
+        $menu->addChild('Clients')
+            ->setAttribute('class','nav-item dropdown')
+            ->setLabelAttribute('class','dropdown-toggle')
+            ->setLabelAttribute('data-toggle','dropdown')
+            ->setChildrenAttribute('class','dropdown-menu');
         $menu['Invoices']->addChild('New Invoice', array('route' => 'new-invoice'));
         $menu['Clients']->addChild('New Client', array('route' => 'new-client'));
+        $menu['Home']->setLinkAttribute('class', 'nav-item');
         return $menu;
     }
 }
