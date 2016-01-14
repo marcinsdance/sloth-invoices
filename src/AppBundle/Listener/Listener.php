614e6a4447a5e74bff1a6ee6fd9ee9c5454f53fc
Marcin <mdancewicz@gmail.com>
2016-01-15 00:01:39 +0100

    Listener completed

--- src/AppBundle/Listener/Listener.php
+++ src/AppBundle/Listener/Listener.php
@@ -3,27 +3,44 @@
 namespace AppBundle\Listener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use WhiteOctober\BreadcrumbsBundle\Model\Breadcrumbs;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class Listener
 {
     protected $twig;
-    protected $request;
+    protected $requestStack;
+    protected $whiteOctoberBreadcrumbsBundle;
+    protected $router;
 
     /**
      * Listener constructor.
      * @param \Twig_Environment $twig
-     * @param Request $request
+     * @param RequestStack $requestStack
      */
-    public function __construct(\Twig_Environment $twig, Request $request)
+    public function __construct(
+        \Twig_Environment $twig,
+        RequestStack $requestStack,
+        Breadcrumbs $whiteOctoberBreadcrumbsBundle,
+        Router $router)
     {
         $this->twig = $twig;
-        $this->request = $request;
+        $this->requestStack = $requestStack;
+        $this->whiteOctoberBreadcrumbsBundle = $whiteOctoberBreadcrumbsBundle;
+        $this->router = $router;
     }
 
     public function onKernelRequest(GetResponseEvent $event)
     {
-        $route = $this->request->getRequestUri();
+        $route = $this->requestStack->getCurrentRequest()
+            ->getPathInfo();
+        $breadcrumbs = $this->whiteOctoberBreadcrumbsBundle;
+        $breadcrumbs->addItem("Home", $this->router->generate("home"));
+        $breadcrumbsEnd = str_replace('/','',$route);
+        $breadcrumbsEnd = ucfirst($breadcrumbsEnd);
+        $breadcrumbs->addItem($breadcrumbsEnd);
+
         $this->twig->addGlobal('myvar', $route);
     }
 }
